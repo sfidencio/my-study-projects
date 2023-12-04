4bcb99bbd1e757ad279cd6487e733d6c20f66255
Sebastiao Fidencio <sfidencio@gmail.com>
2023-12-04 15:02:29 -0300

    -Explorando clean code - ajuste documentacao

--- explorando-clean-code/src/main/java/com/github/sfidencio/exemplo1/proposta1/CalculadoraTeste.java
+++ explorando-clean-code/src/main/java/com/github/sfidencio/exemplo1/proposta1/CalculadoraTeste.java
@@ -3,13 +3,13 @@ package com.github.sfidencio.exemplo1.proposta1;
 public class CalculadoraTeste {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        imprimeResultado(calculadora.calcular(CalculadoraStrategy.DIVISAO, 9, 8));
-        imprimeResultado(calculadora.calcular(CalculadoraStrategy.SOMA, 9, 8));
-        imprimeResultado(calculadora.calcular(CalculadoraStrategy.SUBTRACAO, 9, 8));
-        imprimeResultado(calculadora.calcular(CalculadoraStrategy.MULTIPLICACAO, 9, 8));
+        imprimirResultado(calculadora.calcular(CalculadoraStrategy.DIVISAO, 9, 8));
+        imprimirResultado(calculadora.calcular(CalculadoraStrategy.SOMA, 9, 8));
+        imprimirResultado(calculadora.calcular(CalculadoraStrategy.SUBTRACAO, 9, 8));
+        imprimirResultado(calculadora.calcular(CalculadoraStrategy.MULTIPLICACAO, 9, 8));
     }
 
-    static void imprimeResultado(double resultado) {
+    static void imprimirResultado(double resultado) {
         System.out.println(resultado);
     }
 }
