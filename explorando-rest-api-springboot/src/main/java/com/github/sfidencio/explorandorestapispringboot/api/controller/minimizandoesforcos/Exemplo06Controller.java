133e75a4083a3dffb507285ab5ffb1c1b5e30921
Sebastiao Fidencio <sfidencio@gmail.com>
2023-12-06 19:22:35 -0300

    -Continuando o entendimento REstful no springboot

---
+++ explorando-rest-api-springboot/src/main/java/com/github/sfidencio/explorandorestapispringboot/api/controller/minimizandoesforcos/Exemplo06Controller.java
@@ -0,0 +1,19 @@
+package com.github.sfidencio.explorandorestapispringboot.api.controller.usandorequestmapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("usandorequestmapping.Exemplo02Controller")
+public class Exemplo02Controller {
+    /*
+     * O método exemplo02() é mapeado para a URL /exemplo02
+     * curl  --location http://localhost:8080/exemplo02 -X GET --header 'Content-Type: application/json'
+     */
+    @RequestMapping(value = "/exemplo02", method = RequestMethod.GET)
+    @ResponseBody
+    public String exemplo02() {
+        return "Hello World!";
+    }
+}
