ad0ca861b7fabd92da352c3397dd22548a1f376e
Sebastiao Fidencio <sfidencio@gmail.com>
2024-04-04 17:15:29 -0300

    -Andamento api livros
    -Andamento teste parameters

--- api-livros/src/main/java/com/github/sfidencio/api/dto/LivroDTO.java
+++ api-livros/src/main/java/com/github/sfidencio/api/dto/LivroDTO.java
@@ -1,7 +1,5 @@
 package com.github.sfidencio.api.dto;
 
-import com.github.sfidencio.api.dto.validators.FirstEmptyValidation;
-import com.github.sfidencio.api.dto.validators.SecondValidationRegexValidation;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
@@ -12,8 +10,9 @@ import lombok.*;
 @NoArgsConstructor
 public class LivroDTO {
     private Long id;
-    @SecondValidationRegexValidation(message = "Campo título não pode conter caracteres especiais", regex = "^[a-zA-Z0-9]*$")
-    @FirstEmptyValidation(message = "Campo título é obrigatório")
+    //@SecondValidationRegexValidation(message = "Campo título não pode conter caracteres especiais", regex = "^[a-zA-Z0-9]*$")
+    ///@FirstEmptyValidation(message = "Campo título é obrigatório")
+    @NotEmpty(message = "Campo título é obrigatório")
     private String titulo;
     @NotEmpty(message = "Campo autor é obrigatório")
     private String autor;
